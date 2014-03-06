773defd75b2b6af06ae43386425db42698c6b623
Sacha Delanoue <sacha.delanoue@gmail.com>
2014-03-06 20:22:53 +0100

    Clean code and add time.

--- midi.hs
+++ midi.hs
@@ -1,25 +1,25 @@
-module Midi where
+module Midi (open, Note) where
 
 import Sound.MIDI.File.Load (fromFile)
 import Sound.MIDI.File
-import Data.EventList.Relative.TimeBody (mapMaybe, getBodies)
+import Data.EventList.Relative.TimeBody (mapMaybe, mapTime, toPairList)
 import Sound.MIDI.File.Event (maybeMIDIEvent)
 import Sound.MIDI.Message.Channel (messageBody, Body(Voice))
 import Sound.MIDI.Message.Channel.Voice (T(NoteOn), fromPitch, fromVelocity)
 
-data Music = Music {division :: Division, track :: [Note]}
-    deriving (Show)
-
 type Note = (Int, Int) -- Drum part, intensity.
 
-open :: FilePath -> IO Music
+open :: FilePath -> IO [(Integer, Note)]
 open filename = do
     Cons t d tracks <- fromFile filename
-    -- TODO Get meta events
-    let notes = mapMaybe ((>>= return . messageBody) . maybeMIDIEvent) $ mergeTracks t tracks
-    -- TODO keep time
-    return $ Music d $ getBodies $ mapMaybe bodyToNote notes
+    let notes = mapMaybe bodyToNote $ getMessages $ mergeTracks t tracks
+    return $ toPairList $ mapTime fromElapsedTime notes
+    where
+        -- Lose: meta events, system exclusive information, channel number.
+        getMessages = mapMaybe ((>>= return . messageBody) . maybeMIDIEvent)
 
+-- | Get note information (pitch and velocity) from a MIDI channel message.
+-- Lose: all channel messages other than NoteOn (Mode information, NoteOff…).
 bodyToNote :: Body -> Maybe Note
 bodyToNote (Voice (NoteOn p v)) = Just (fromPitch p, fromVelocity v)
 bodyToNote _                    = Nothing
