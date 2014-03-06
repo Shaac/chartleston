368ee52edbea7d60d34d63266a2c0c9c10e0bfab
Sacha Delanoue <sacha.delanoue@gmail.com>
2014-03-06 02:09:48 +0100

    Parse the MIDI format.

--- midi.hs
+++ midi.hs
@@ -2,11 +2,24 @@ module Midi where
 
 import Sound.MIDI.File.Load (fromFile)
 import Sound.MIDI.File
+import Data.EventList.Relative.TimeBody (mapMaybe, getBodies)
+import Sound.MIDI.File.Event (maybeMIDIEvent)
+import Sound.MIDI.Message.Channel (messageBody, Body(Voice))
+import Sound.MIDI.Message.Channel.Voice (T(NoteOn), fromPitch, fromVelocity)
 
-data Music = Music {division :: Division, track :: Track}
+data Music = Music {division :: Division, track :: [Note]}
     deriving (Show)
+
+type Note = (Int, Int) -- Drum part, intensity.
 
 open :: FilePath -> IO Music
 open filename = do
     Cons t d tracks <- fromFile filename
-    return $ Music d $ mergeTracks t tracks
+    -- TODO Get meta events
+    let notes = mapMaybe ((>>= return . messageBody) . maybeMIDIEvent) $ mergeTracks t tracks
+    -- TODO keep time
+    return $ Music d $ getBodies $ mapMaybe bodyToNote notes
+
+bodyToNote :: Body -> Maybe Note
+bodyToNote (Voice (NoteOn p v)) = Just (fromPitch p, fromVelocity v)
+bodyToNote _                    = Nothing
