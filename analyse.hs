5aa4e0855fb42b71eb956d0016b9c78b7ad408de
Sacha Delanoue <sacha.delanoue@gmail.com>
2014-03-20 11:37:11 +0100

    Use a smaller indentation.

--- analyse.hs
+++ analyse.hs
@@ -5,22 +5,22 @@ import Midi (Note)
 equalise :: [Integer] -> [Integer]
 equalise [] = []
 equalise l = let (a, b) = split l in treat a ++ (equalise b)
-    where
-        split []     = ([], []) -- Not a possible input, but avoid warnings.
-        split (x:xs) = mapFst (x:) $ span ((< 0.2) . (ratio x)) xs
-        ratio x y    = abs $ fromInteger y / (fromInteger x) - 1 :: Rational
-        treat x      = let s = length x in replicate s ((sum x) `div` (fromIntegral s))
+  where
+    split []     = ([], []) -- Not a possible input, but avoid warnings.
+    split (x:xs) = mapFst (x:) $ span ((< 0.2) . (ratio x)) xs
+    ratio x y    = abs $ fromInteger y / (fromInteger x) - 1 :: Rational
+    treat x      = let s = length x in replicate s ((sum x) `div` (fromIntegral s))
 
 join :: [(Integer, Note)] -> [(Integer, [Note])]
 join = mergeZeros (0, []) . (setZeros 0)
-   where
-        setZeros _ []   = []
-        setZeros t ((time, note):xs)
-            | time < 20 = (0, note) : (setZeros (time + t) xs)
-            | otherwise = (time + t, note) : (setZeros 0 xs)
-        mergeZeros a [] = [a]
-        mergeZeros (a1,a2) ((0,x):xs) = mergeZeros (a1, x:a2) xs
-        mergeZeros a ((x1,x2):xs) = a : (mergeZeros (x1,[x2]) xs)
+  where
+    setZeros _ []   = []
+    setZeros t ((time, note):xs)
+      | time < 20 = (0, note) : (setZeros (time + t) xs)
+      | otherwise = (time + t, note) : (setZeros 0 xs)
+    mergeZeros a [] = [a]
+    mergeZeros (a1,a2) ((0,x):xs) = mergeZeros (a1, x:a2) xs
+    mergeZeros a ((x1,x2):xs) = a : (mergeZeros (x1,[x2]) xs)
 
 shiftFst :: [(a, b)] -> [(a, b)]
 shiftFst = uncurry zip . (mapFst (drop 1 . cycle)) . unzip
--- midi.hs
+++ midi.hs
@@ -14,15 +14,15 @@ type Note = (Int, Int) -- Drum part, intensity.
 -- | Open a MIDI file, parse it, and return the notes list.
 open :: FilePath -> IO [(Integer, Note)]
 open filename = do
-    -- Read file.
-    Cons t _ tracks <- fromFile filename -- Lose: division.
-    -- Parse the MIDI files to get the notes.
-    let notes = mapMaybe bodyToNote $ getMessages $ mergeTracks t tracks
-    -- Retun the pairs time, note.
-    return $ toPairList $ mapTime fromElapsedTime notes
-    where
-        -- Lose: meta events, system exclusive information, channel number.
-        getMessages = mapMaybe (liftM messageBody . maybeMIDIEvent)
+  -- Read file.
+  Cons t _ tracks <- fromFile filename -- Lose: division.
+  -- Parse the MIDI files to get the notes.
+  let notes = mapMaybe bodyToNote $ getMessages $ mergeTracks t tracks
+  -- Retun the pairs time, note.
+  return $ toPairList $ mapTime fromElapsedTime notes
+  where
+    -- Lose: meta events, system exclusive information, channel number.
+    getMessages = mapMaybe (liftM messageBody . maybeMIDIEvent)
 
 -- | Get note information (pitch and velocity) from a MIDI channel message.
 -- Lose: all channel messages other than NoteOn (Mode information, NoteOff…).
