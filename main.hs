25968afe801cd3606fee4a33b53bbd6490c65ff6
Sacha Delanoue <sacha.delanoue@gmail.com>
2014-04-07 16:05:56 +0200

    Use all libraries into a program.

---
+++ main.hs
@@ -0,0 +1,11 @@
+import System.Environment (getArgs)
+
+import Analyse (analyse)
+import Midi    (open)
+import Write   (write)
+
+main :: IO ()
+main = do
+    args <- getArgs
+    midi <- open $ args !! 0
+    print $ write $ analyse $ midi
