55eebb90b061f23ce37738ad83bc91badbe1b019
Sacha Delanoue <sacha.delanoue@gmail.com>
2014-05-13 00:25:28 +0200

    Add a function that slice into measures.

---
+++ structure.hs
@@ -0,0 +1,12 @@
+module Structure where
+
+import Data.Ratio (denominator)
+
+measures :: [(Integer, a)] -> [[(Integer, a)]]
+measures = measure 0
+  where
+    measure _   []            = []
+    measure acc (x@(t, _):xs) = let s = (1 / (fromInteger t) + acc) in
+      if denominator s == 1
+        then [x] : (measure 0 xs)
+        else let (a : b) = measure s xs in (x : a) : b
