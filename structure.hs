30350949ab6f722e99850c62532cab0004bd0418
Sacha Delanoue <sacha.delanoue@gmail.com>
2014-05-14 13:16:11 +0200

    Avoid “Irrefutable pattern failed” error.

--- structure.hs
+++ structure.hs
@@ -9,4 +9,6 @@ measures = measure (0 :: Rational)
     measure acc (x@(t, _):xs) = let s = (1 / (fromInteger t) + acc) in
       if denominator s == 1
         then [x] : (measure 0 xs)
-        else let (a : b) = measure s xs in (x : a) : b
+        else let l = measure s xs in
+          if length l == 0 then [x] : (measure 0 xs) -- ERR: malformed measure!
+          else (x : (head l)) : (tail l)
