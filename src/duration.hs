221c94c3fd745cb86a8bdb7a6a8f7f9b2d1f32d9
Sacha Delanoue <sacha.delanoue@gmail.com>
2014-06-12 03:45:18 +0200

    Fix use of long notes.

--- src/duration.hs
+++ src/duration.hs
@@ -205,7 +205,7 @@ keep n = (map snd) . (take n) . sort . (map compute) . (mapMaybe remove)
       where x = (ok xs) ++ a
     split a acc xs
       | a == 0.25 = Just (reverse acc, xs)
-      | a >  0.25 = if length acc == 1 then Just (acc, xs) else Nothing
+      | a >  0.25 = if (duration $ value $ head acc) > (0.25 :: Rational) then split (a - 0.25) acc xs else Nothing
       | otherwise = case xs of
         []      -> Just ([], reverse acc)
         (x:xs') -> split (a + (duration $ value $ x)) (x : acc) xs'
