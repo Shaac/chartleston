ba939f799633b75df514ac7fb4687494ddf5dc4e
Sacha Delanoue <sacha.delanoue@gmail.com>
2014-06-20 15:54:42 +0200

    Clean Analyse module; separate constants.

--- src/analyse.hs
+++ src/analyse.hs
@@ -6,6 +6,27 @@ import Control.Arrow (first, (***), (&&&))
 import Duration (Duration, guess)
 
 
+---------------
+-- Constants --
+---------------
+
+-- Maximal ratio of two durations for them to be equalised.
+equal :: Rational
+equal = 0.2
+
+-- Minimal duration, in seconds, of the silence at the beginning.
+minimalRest :: Rational
+minimalRest = 0.5
+
+-- Duration, in seconds, of an ideal semibreve.
+semibreve :: Rational
+semibreve = 3
+
+-- Maximal duration, in seconds, of two simultaneous notes.
+simultaneous :: Rational
+simultaneous = 0.075
+
+
 ------------------------
 -- Exported functions --
 ------------------------
@@ -13,8 +34,7 @@ import Duration (Duration, guess)
 -- | Analyse a list of real notes and organise them with regular durations.
 analyse :: (RealFrac a, Ord a) => [(a, b)] -> [(Duration, [b])]
 analyse = uncurry zip . first treat . unzip . join
-  where
-    treat = lastNote . detect . normalise . equalise . drop 1
+  where treat = lastNote . detect . normalise . equalise . drop 1
 
 
 ---------------------
@@ -27,14 +47,14 @@ lastNote xs = xs ++ [1 - ((snd :: (Int, a) -> a) $ properFraction $ sum xs)]
 
 -- Detect the notes durations.
 detect :: RealFrac a => [(a, a)] -> [Duration]
-detect =  guess . map snd
+detect = guess . map snd
 
 -- Normalise the duration list so that it represents the fraction of a measure.
 normalise :: RealFrac a => [(a, a)] -> [(a, a)]
 normalise xs = map ((/ norm) *** (/ norm)) xs
   where
-    norm     = let x = majority (map fst xs) in x * (closest $ 3 / x)
-    majority = snd . maximum . map (length &&& head) . group . sort
+    norm     = let x = majority xs in closest (fromRational semibreve / x) * x
+    majority = snd . maximum . map (length &&& head) . group . sort . map fst
     closest x
       | x <= 2    = max 1 $ fromInteger $ round x
       | otherwise = 2 * (closest (x / 2))
@@ -45,8 +65,8 @@ equalise :: (Fractional a, Ord a) => [a] -> [(a, a)]
 equalise durations = zip (equalise' durations) durations
   where
     equalise' [] = []
-    equalise' xs = uncurry (($) . (++)) $ level *** equalise' $ cut xs
-    cut       xs = span ((< 0.2) . abs . (1 -) . (/ (head xs))) xs
+    equalise' xs = uncurry (++) $ level *** equalise' $ cut xs
+    cut       xs = span ((< fromRational equal) . abs . (1 -) . (/ head xs)) xs
     level     xs = let s = length xs in replicate s $ sum xs / (fromIntegral s)
 
 -- Join notes that are close into simultaneous notes.
@@ -54,13 +74,13 @@ join :: (Fractional a, Ord a) => [(a, b)] -> [(a, [b])]
 join = initialRest . join'
   where
     initialRest xs -- If the first note is a substantial rest, add it.
-      | null $ takeWhile ((> 0.5) . fst) xs = xs
-      | otherwise                           = (0, []) : xs
+      | null $ takeWhile ((> fromRational minimalRest) . fst) xs = xs
+      | otherwise                                                = (0, []) : xs
     join' []            = []
     join' (x : xs)      = ((+ offset) *** (: map snd simult)) x : join' rest
       where
-        (simult, after) = span ((< 0.075) . fst) xs
+        (simult, after) = span ((< fromRational simultaneous) . fst) xs
         rest = case after of
-          []       -> []
-          (y : ys) -> first (+ offset) y : ys
+          []            -> []
+          (y : ys)      -> first (+ offset) y : ys
         offset          = (/ 2) $ sum $ map fst simult
