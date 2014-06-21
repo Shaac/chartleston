ed7477f457ca3424ec9cca0e462a3928f6d10026
Sacha Delanoue <sacha.delanoue@gmail.com>
2014-06-21 18:09:45 +0200

    Comment Score module.

--- src/score.hs
+++ src/score.hs
@@ -3,6 +3,12 @@ module Score (Measure (..), Score (..)) where
 import Duration (Duration)
 import Note     (Note)
 
+
+---------------
+-- Structure --
+---------------
+
+-- Represent the work in progress.
 data Score a = Score {
     title :: String,
     score :: a
@@ -11,6 +17,7 @@ data Score a = Score {
 instance Functor Score where
   fmap f s = s { score = f (score s) }
 
+-- Represent a measure, and how it is repeated.
 data Measure = Measure {
     measure :: ([(Duration, [Note])], [(Duration, [Note])]),
     nRepeat :: Int
